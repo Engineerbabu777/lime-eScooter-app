e2fce7faaae9e030175683f7fc3d6cc2fc73eb54
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-11-14 19:08:11 +0500

    add(bottom-sheet): a good looking bottom sheet has been added

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,9 @@
 import { Stack } from 'expo-router';
 
-import { StatusBar } from 'react-native';
+import { StatusBar, StyleSheet, Text } from 'react-native';
 
 import Map from '@/components/Map';
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
 
 export default function Home() {
   return (
@@ -10,6 +11,24 @@ export default function Home() {
       <Stack.Screen options={{ title: 'Home', headerShown: false }} />
       <Map />
       <StatusBar barStyle={'light-content'} />
+
+      <BottomSheet snapPoints={[200]} enablePanDownToClose>
+        <BottomSheetView style={styles.contentContainer}>
+          <Text>Awesome 🎉</Text>
+        </BottomSheetView>
+      </BottomSheet>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'grey',
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 36,
+    alignItems: 'center',
+  },
+});
