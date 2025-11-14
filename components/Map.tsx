6ca0dda35edcb0e898017f2cea39aad83b7a75e3
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-11-14 11:11:31 +0500

    add(location): showing user location is done on map

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,5 +1,5 @@
 import { Text, View } from 'react-native';
-import Mapbox, { MapView } from '@rnmapbox/maps';
+import Mapbox, { Camera, LocationPuck, MapView } from '@rnmapbox/maps';
 
 const accessToken =
   'pk.eyJ1IjoiZW5naW5lZXJhd2Fpczc3NyIsImEiOiJjbWV2Mnh3Z2cwNmwwMmlzN3ZqaXNjaTRyIn0.L_4Cf2FmaoRLYlZch0RGhw';
@@ -7,5 +7,11 @@ const accessToken =
 Mapbox.setAccessToken(accessToken);
 
 export default function Map() {
-  return <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/dark-v11" />;
+  return (
+    <MapView style={{ flex: 1 }} styleURL="mapbox://styles/mapbox/dark-v11">
+      <Camera followUserLocation />
+
+      <LocationPuck />
+    </MapView>
+  );
 }
