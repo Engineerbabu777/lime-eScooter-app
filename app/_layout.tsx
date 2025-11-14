0773f52eb47710babb6948f56f0e011656a1a9d6
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-11-14 18:51:31 +0500

    add(codes): few number of chnages have been made

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,13 @@
 import ScooterProvider from '@/providers/scooter-provider';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Stack } from 'expo-router';
 
 export default function Layout() {
   return (
-    <ScooterProvider>
-      <Stack />
-    </ScooterProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ScooterProvider>
+        <Stack />
+      </ScooterProvider>
+    </GestureHandlerRootView>
   );
 }
