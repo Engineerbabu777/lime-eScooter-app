c9142cdaa74c7b1c10d8a27d47825f3162f52a31
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-11-15 19:03:04 +0500

    feat(auth): this is done

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -4,21 +4,13 @@ import { StatusBar } from 'react-native';
 
 import Map from '@/components/Map';
 import SelectedScooterSheet from '@/components/SelectedScooterSheet';
-import { useAuthProvider } from '@/providers/AuthProvider';
-import { Button } from '@/components/Button';
-import { supabase } from '@/lib/supabse';
 
 export default function Home() {
-  const { session } = useAuthProvider();
-
-  console.log({ session });
-
   return (
     <>
       <Stack.Screen options={{ title: 'Home', headerShown: false }} />
       <Map />
       <StatusBar barStyle={'light-content'} />
-      <Button onPress={() => supabase.auth.signOut()} title="SignOut" />
 
       <SelectedScooterSheet />
     </>
