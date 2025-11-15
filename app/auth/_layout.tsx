55ce6091c3d049716fb762ada404b9ccdd62350f
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-11-15 19:02:00 +0500

    feat(auth): this is done

---
+++ app/auth/_layout.tsx
@@ -0,0 +1,16 @@
+import { useAuthProvider } from '@/providers/AuthProvider';
+import { Redirect, Slot } from 'expo-router';
+
+export default function HomeLayout() {
+  const { isAuthenticated } = useAuthProvider();
+
+  if (isAuthenticated) {
+    return <Redirect href={'/(home)'} />;
+  }
+
+  return (
+    <>
+      <Slot />
+    </>
+  );
+}
