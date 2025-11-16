7e6f149c450f4ea5cdf3d7292107994911be3144
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-11-16 23:23:10 +0500

    add(track-route): tract ride route and stored in DB

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,4 @@
-function chunkArray<T>(arr: T[], size: number): T[][] {
+export function chunkArray<T>(arr: T[], size: number): T[][] {
   const chunks = [];
   for (let i = 0; i < arr.length; i += size) {
     chunks.push(arr.slice(i, i + size));
